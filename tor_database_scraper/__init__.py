f7a6c49a05c47ddc3cb068f7bc56e5d61de6ffa8
Tim Jentzsch <tim-jentzsch@gmx.de>
2022-03-31 22:04:11 +0200

    Add scraper for volunteers

--- tor_database_scraper/__init__.py
+++ tor_database_scraper/__init__.py
@@ -1,4 +1,6 @@
-from typing import Dict
+import json
+import os
+from typing import Dict, Any
 
 import psycopg2
 
@@ -6,6 +8,15 @@ from tor_database_scraper.parsers import parse_volunteer
 from tor_database_scraper.scrapers import scrape_volunteers
 
 
+def _dump(obj: Any, name: str):
+    if not os.path.exists("output"):
+        os.makedirs("output")
+
+    with open(f"output/{name}.json", "w+") as file:
+        json.dump(obj, file)
+
+
 def scrape_db(postgres_cfg: Dict):
     with psycopg2.connect(**postgres_cfg) as connection:
-        scrape_volunteers(connection)
+        volunteers = scrape_volunteers(connection)
+        _dump(volunteers, "volunteers")
--- tor_database_scraper/scrapers.py
+++ tor_database_scraper/scrapers.py
@@ -1,7 +1,10 @@
+from typing import List
+
 from tor_database_scraper import parse_volunteer
+from tor_database_scraper.types import Volunteer
 
 
-def scrape_volunteers(connection):
+def scrape_volunteers(connection) -> List[Volunteer]:
     with connection.cursor() as cursor:
         cursor.execute(
             """
@@ -12,10 +15,14 @@ def scrape_volunteers(connection):
                 WHERE completed_by_id IS NOT NULL
                 GROUP BY completed_by_id
             ) as g ON u.id = g.completed_by_id
-            WHERE u.id = 4034
+            ORDER BY u.date_joined;
             """
         )
 
-        for row in [cursor.fetchone()]:
+        volunteers = []
+
+        for row in cursor.fetchall():
             volunteer = parse_volunteer(*row)
-            print(volunteer)
+            volunteers.append(volunteer)
+
+        return volunteers
